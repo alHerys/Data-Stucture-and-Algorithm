8813146e82b23e6c66162971d8276a2762b637ae
Alvianto Hery Sarborn <alfiantoherysarborn@gmail.com>
2025-11-13 14:29:31 +0700

    final

--- src/praktikum/pretest/graph/latihan/Main.java
+++ src/praktikum/pretest/graph/latihan/Main.java
@@ -2,7 +2,9 @@ package praktikum.pretest.graph.latihan;
 
 public class Main {
     public static void main(String[] args) {
+
         Graph g = new Graph(5);
+
         g.addAdj(0, 3);
         g.addAdj(0, 1);
         g.addAdj(1, 4);
@@ -10,6 +12,23 @@ public class Main {
         g.addAdj(2, 4);
         g.addAdj(2, 1);
         g.addAdj(4, 3);
-        g.cetak("Kondisi awal");
+
+        g.cetak("Representasi Adjacency List:");
+        System.out.println("---------------------------------");
+
+        System.out.println("--- Menguji Method Latihan ---");
+
+        System.out.println("Node 1:");
+        System.out.println("  Jumlah Edge Keluar (Out-Degree): " + g.getJumlahEdgeKeluar(1));
+        System.out.println("  Jumlah Tetangga: " + g.getJumlahTetangga(1));
+        System.out.println("  Jumlah Edge Masuk (In-Degree): " + g.getJumlahEdgeMasuk(1));
+
+        System.out.println("\nNode 3:");
+        System.out.println("  Jumlah Edge Keluar (Out-Degree): " + g.getJumlahEdgeKeluar(3));
+        System.out.println("  Jumlah Edge Masuk (In-Degree): " + g.getJumlahEdgeMasuk(3));
+
+        System.out.println("\nNode 4:");
+        System.out.println("  Jumlah Edge Keluar (Out-Degree): " + g.getJumlahEdgeKeluar(4));
+        System.out.println("  Jumlah Edge Masuk (In-Degree): " + g.getJumlahEdgeMasuk(4));
     }
 }
